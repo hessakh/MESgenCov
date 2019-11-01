51aaf345c1b64a5a092f014bc09fe2af998e89d5
Hessa Al-Thani <hessakh@umich.edu>
2019-11-01 15:46:30 -0400

    -test input

---
+++ R/testInput.R
@@ -0,0 +1,21 @@
+# Trial input for tests for getCov and getCov2
+
+weeklyB           <- FALSE
+startdateStr      <- "01/01/83 00:00"
+enddateStr        <- "12/31/86 00:00"
+use36             <- TRUE
+siteAdd           <- NULL
+outliersDates     <- NULL
+outlierDatesbySite <- NULL
+showOutliers      <- FALSE
+siteOutliers      <- NULL
+comp              <- "SO4"
+plotMulti         <- TRUE
+plotB             <- FALSE
+sitePlot          <- NULL
+plotAll           <- FALSE
+writeMat          <- FALSE
+seas              <- 12
+r                 <- 1
+k                 <- 1
+p                 <- 1

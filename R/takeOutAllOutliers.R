f4ca7c1ec6fee29ddaac2d9d0bb9384b305af639
Hessa Al-Thani <hessakh@umich.edu>
2020-02-11 18:27:04 -0500

    - new internal function that takes the rosner test result and turns it into input that can be interpreted by getCov(), ignore description for now

---
+++ R/takeOutAllOutliers.R
@@ -0,0 +1,29 @@
+#'@keywords internal
+#' returns LambertW transformed residuals
+#' @param dfRes       dataframe, 
+#' @param plotMulti   
+
+#' @return             Returns a dataframe of LambertW transformed residuals
+
+
+
+takeOutAllOutliers <- function(sites, rosnerResult,cati){
+
+  datesBySiteVec <- NULL
+  outSites       <- NULL
+  for(i in 1:length(sites)){
+    index1 <- match(sites[i],cati)
+    stats  <- rosnerResult[[index1]]$all.stats
+    outliers <- stats[stats$Outlier == TRUE,]
+    if(dim(outliers)[1] > 0){
+      outlierst <- outliers$Obs.Num
+      outSites  <- c(outSites, sites[i])
+      datesBySiteVec <- c(datesBySiteVec, cati[index1])
+      for (j in outlierst){
+        datesBySiteVec <- c(datesBySiteVec, as.numeric(j))
+      }
+    }
+  }
+  list_1 <- list("outBySite" = datesBySiteVec, "sites" = outSites)
+  return(list_1)
+}

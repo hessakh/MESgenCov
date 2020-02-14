6dd4c37d0921a422debf1466aa40d371baea8666
Hessa Al-Thani <hessakh@umich.edu>
2020-02-14 09:14:23 -0500

    -re-wrote code so that it would be simpler and easier to see errors

--- R/aggregateMonthly.R
+++ R/aggregateMonthly.R
@@ -2,69 +2,29 @@
 #'This function takes a dataframe with weekly precipitation and concentration values,
 #'
 aggregateMonthly <-
-function (binary,site,siObs,obs,obsi,totT,strtYrMo){#binary con or pH
-   	  sitem <- data.frame(matrix(ncol=3, nrow=totT))
-	    yrm        <- site[1,5]
-	    strtYrMo   <- strtoi(strtYrMo)
-	    sitem[1,1] <- strtYrMo
-	    mStrt      <- ((strtYrMo) - floor(strtYrMo/100)*100)
-	    yrdiff     <- (floor(yrm/100)- floor(strtYrMo/100))
-	    mdiff      <- (yrm - floor(yrm/100)*100) - ((strtYrMo) - floor(strtYrMo/100)*100)
-	    diffYrm    <- 12*yrdiff + mdiff
-
-	    sitem[,2]  <- 1:totT   #t
-      #fill in yrmonth
-	    for(i in 1:totT){
-	      sitem[i,1] <- strtYrMo + 100*(floor((i+mStrt-2)/12)) + ((i+mStrt-2)%%12)
-	      sitem[i,3] <- NA
-	    }
-      #fill in deposit values
-	    sitem[diffYrm + 1 ,3] <- 0   #SO4 vol
-	    lyrm <- site[dim(site)[1],5] #last month
-
-	    j = diffYrm + 1
-	    c <- 1 #counter of weeks in month
-	    ma <- site[1,7 + siObs] #monthly aggregate
-
-    	if (binary == 1){
-			for (i in 2:dim(site)[1]){
-		      if(site[i,5] == yrm){
-		        ma <- ma + site[i,7 + siObs]
-		        c <- c +1
-		        if (yrm == lyrm && i == nrow(site)){
-		          sitem[j,3] <- ma/c
-		        }
-		      } else if(site[i,5] > yrm){
-		      	#monthly aggregate / recorded perciptation over the month
-		        sitem[j,3] <- ma/c
-		        c <- 1
-		        ma <- site[i,7 + siObs]
-		        yrm <- site[i,5]
-		        j <- match(yrm, sitem[,1])
-		      }
-		    }
-    	}else{
-	    for (i in j:nrow(site)){
-	      yrdiff     <- floor(site[i,5]/100)-floor(yrm/100)
-	      if(site[i,5] == yrm){
-	        ma <- ma + site[i,7+siObs]
-	        c <- c +1
-	        if (yrm == lyrm && i == nrow(site)){
-	          sitem[j,3] <- ma/sum(site[(i-c+1):i,6 + siObs])
-	        }
-	      } else if(site[i,5] > yrm){ #if row yrm is > curr yrm wrap up last yrm
-	      	#monthly aggregate / recorded perciptation over the month
-	      	if(sum(site[(i-c):(i-1),6 + siObs]) == 0){sump = NA}
-	      	else{sump = sum(site[(i-c):(i-1),6 + siObs])} #sum of precepitation in month
-	        sitem[j,3] <- ma/sump
-	        #re-intialize monthly vars
-	        c <- 1
-	        ma  <- site[i,7 + siObs]
-	        yrm <- site[i,5]
-	        j <- match(yrm, sitem[,1])
-	      }
-	    }
-	}
-	colnames(sitem) <- c("yrmonth", "t", paste(obs[obsi],"Con",sep=""))
-	sitem
+function (bi,site,siObs,obs,obsi,totT,strtYrMo,diffyrm1){#binary con or pH
+  sitem <- data.frame(matrix(ncol=3, nrow=totT))
+  yrm        <- site[1,5]
+  strtYrMo   <- strtoi(strtYrMo)
+  mStrt      <- (strtYrMo)%%12
+  
+  #fill in t
+  sitem[,2]  <- 1:totT
+  #fill in yrmonth
+  for(i in 1:totT){
+    sitem[i,1] <- strtYrMo + 100*(floor((i+mStrt-2)/12)) + ((i+mStrt-2)%%12)
+    sitem[i,3] <- NA
+  }
+  for (i in 0:(diffyrm1-2+mStrt)){# handles case with no precip data for month and skips in months
+    siteyrmonth <- site[site$yrmonth==strtYrMo-mStrt+1+(i%%12)+((i)%/%12)*100,]
+    if(sum(siteyrmonth[,obsi + 6+bi]) == 0){
+      sitem[i+1,3] <- NA
+    }else{
+      sitem[i+1,3] <- (sum(siteyrmonth[,obsi+7+bi])/sum(siteyrmonth[,obsi+6+bi]))
+    } #col obsi+7 is volume of comp, col obsi+6 is precipitation amount fone one comp analysis
+  }
+  if(bi){
+    colnames(sitem) <- c("yrmonth", "t", paste("H+ ","Con",sep=""))
+  }else{colnames(sitem) <- c("yrmonth", "t", paste(obs[obsi],"Con",sep=""))}
+  sitem
 }
